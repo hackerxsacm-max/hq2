4a88cc10ef1f8a7729c49297e21cdff30c2a6818
hackerxsacm-max <hackerxsacm@gmail.com>
2025-11-05 19:11:47 +0500

    Update admin.js

--- Rudra/commands/admin.js
+++ Rudra/commands/admin.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "admin",
 	version: "1.0.5",
 	hasPermssion: 0, 
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "🌺💚⭐_₷ขԲ𝔂𝓪𝓃_🌺💚⭐",
 	description: "Manage bot admin",
 	commandCategory: "config",
 	usages: "[list/add/remove] [userID]",
