f9af2fdb5b7527b15c3bae691c263e4da8bb9b7a
hackerxsacm-max <hackerxsacm@gmail.com>
2025-11-05 19:06:35 +0500

    Update acp.js

--- Rudra/commands/acp.js
+++ Rudra/commands/acp.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "acp",
   version: "1.0.0",
   hasPermssion: 2,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "🌺💚⭐_₷ขԲ𝔂𝓪𝓃_🌺💚⭐",
   description: "Make friends via Facebook id",
   commandCategory: "bot id",
   usages: "uid",
