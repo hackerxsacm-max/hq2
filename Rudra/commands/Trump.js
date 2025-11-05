0fb290591d76ee7823f2c3f2c702dc65723ae923
hackerxsacm-max <hackerxsacm@gmail.com>
2025-11-05 19:03:57 +0500

    Update Trump.js

--- Rudra/commands/Trump.js
+++ Rudra/commands/Trump.js
@@ -4,7 +4,7 @@ module.exports.config = {
 
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "🌺💚⭐_₷ขԲ𝔂𝓪𝓃_🌺💚⭐",
 	description: "Comment on the board ( ͡° ͜ʖ ͡°)",
 	commandCategory: "edit-img",
 	usages: "trump [text]",
