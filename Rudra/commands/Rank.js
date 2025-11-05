543c4ae58f5691b61eacde69c455a5909f821207
hackerxsacm-max <hackerxsacm@gmail.com>
2025-11-05 19:00:26 +0500

    Update Rank.js

--- Rudra/commands/Rank.js
+++ Rudra/commands/Rank.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "rank",
 	version: "2.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "🌺💚⭐_₷ขԲ𝔂𝓪𝓃_🌺💚⭐",
 	description: "View Member Rankings",
 	commandCategory: "Group",
 	usages: " [user] or [tag]",
